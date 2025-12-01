e41cedbff92df208cf20c3a2ad7d82aafeaa5983
Nico <nico_verdeille@hotmail.com>
2025-12-01 08:48:36 +0100

    day 01: scratch

---
+++ README.md
@@ -0,0 +1,11 @@
+# Advent of code 2025
+[Advent of Code website](https://adventofcode.com)
+
+## Day 01
+Language: Scratch
+
+How to launch:
+* Load program
+* Import `input` into list `input`
+* Click on the green flag
+
---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Advent of code 2025";
+
+  inputs = {
+    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rust = pkgs.rust-bin.stable."1.82.0".minimal.override {
+          extensions = [ "rustfmt" "clippy" "rust-src" "rust-analyzer" ];
+        };
+      in
+      with pkgs;
+      {
+        devShells.default = mkShell.override { stdenv = stdenvNoCC; } {
+          buildInputs = [
+          ];
+        };
+      }
+    );
+}
